99592e7278f73258b1fc167b0012cd23816fe86b
sammy ocharo <obanyisammy@gmail.com>
2023-06-05 07:22:25 +0300

    add school deactivate endpoint by admin

--- portal_schools_api/serializers.py
+++ portal_schools_api/serializers.py
@@ -5,3 +5,8 @@ class AdminCreateSchoolSerializer(serializers.Serializer):
     school_name = serializers.CharField()
     school_dean = serializers.CharField()
     school_description = serializers.CharField()
+
+class AdminDeactivateSchoolSerializer(serializers.Serializer):
+    admin_email = serializers.EmailField()
+    school_code = serializers.CharField()
+    school_name = serializers.CharField()
--- portal_schools_api/urls.py
+++ portal_schools_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-create-school/', views.AdminCreateSchoolAPIView.as_view(), name='admin-create-school-api')
+    path('admin-create-school/', views.AdminCreateSchoolAPIView.as_view(), name='admin-create-school-api'),
+    path('admin-deactivate-school/', views.AdminDeactivateSchoolAPIView.as_view(), name='admin-deactivate-school-api'),
 ]
