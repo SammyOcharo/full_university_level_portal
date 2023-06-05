6ac827eba511906db7cce180870d28b6ba066626
sammy ocharo <obanyisammy@gmail.com>
2023-06-05 07:30:29 +0300

    add school activate endpoint by admin

--- portal_schools_api/urls.py
+++ portal_schools_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin-create-school/', views.AdminCreateSchoolAPIView.as_view(), name='admin-create-school-api'),
+    path('admin-activate-school/', views.AdminActivateSchoolAPIView.as_view(), name='admin-create-school-api'),
     path('admin-deactivate-school/', views.AdminDeactivateSchoolAPIView.as_view(), name='admin-deactivate-school-api'),
 ]
