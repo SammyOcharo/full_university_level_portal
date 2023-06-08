afba8d3f2035eaa14f2581d169c9b17e3ef4c701
sammy ocharo <obanyisammy@gmail.com>
2023-06-08 07:31:57 +0300

    add get all departments restendpoints

--- portal_school_department_api/urls.py
+++ portal_school_department_api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("admin-activate-school-department/", views.AdminActivateSchoolDepartmentAPIView.as_view(), name='admin-activate-school-department-api'),
     path("admin-deactivate-school-department/", views.AdminDeactivateSchoolDepartmentAPIView.as_view(), name='admin-deactivate-school-department-api'),
     path("admin-delete-school-department/", views.AdminDeleteSchoolDepartmentAPIView.as_view(), name='admin-delete-school-department-api'),
-
+    path("admin-view-all-school-departments/", views.AdminViewAllSchoolDepartmentsAPIView.as_view(), name='admin-delete-school-department-api'),
 
 ]
