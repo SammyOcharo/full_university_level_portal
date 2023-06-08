027359146c88b492a8f33cdebc2004063eb20252
sammy ocharo <obanyisammy@gmail.com>
2023-06-08 07:52:39 +0300

    admin set up accounts app

--- portal_accounts_api/urls.py
+++ portal_accounts_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    # path()
+    path("admin-create-accounts/", views.AdminCreateAccountsAPIView.as_view(), name='admin-create-school-department-api'),
 ]
