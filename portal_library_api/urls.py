1ed8668f7793387dad0a03731ff02ab9885698aa
sammy ocharo <obanyisammy@gmail.com>
2023-06-17 22:38:42 +0300

    add activate library admin and tested

--- portal_library_api/urls.py
+++ portal_library_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('admin-create-library-admin/', views.AdminCreateLibraryAdminAPIView.as_view(), name='admin-create-library-admin-api'),
-    path('admin-activate-library-admin/', views.AdminCreateLibraryAdminAPIView.as_view(), name='admin-create-library-admin-api'),
+    path('admin-activate-library-admin/', views.AdminActivateLibraryAdminAPIView.as_view(), name='admin-activate-library-admin-api'),
 ]
