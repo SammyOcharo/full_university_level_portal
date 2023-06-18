9f1948c859f4ce7510b1e6b542c2babf6f5a4372
sammy ocharo <obanyisammy@gmail.com>
2023-06-18 22:48:06 +0300

    add admin add books endpoint

--- portal_library_api/urls.py
+++ portal_library_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('admin-create-library-admin/', views.AdminCreateLibraryAdminAPIView.as_view(), name='admin-create-library-admin-api'),
     path('admin-activate-library-admin/', views.AdminActivateLibraryAdminAPIView.as_view(), name='admin-activate-library-admin-api'),
+    path('admin-add-books/', views.AdminAddBooksAPIView.as_view(), name='admin-add-books-api')
 ]
