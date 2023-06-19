9d63e90483ad02b6ce0afceb126f0c25d0f76556
sammy ocharo <obanyisammy@gmail.com>
2023-06-19 07:32:49 +0300

    add more fields to the library books model

--- portal_library_api/admin.py
+++ portal_library_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from portal_library_api.models import LibraryAdmin
+from portal_library_api.models import LibraryAdmin, LibraryBooks
 
 # Register your models here.
 
 admin.site.register(LibraryAdmin)
+admin.site.register(LibraryBooks)
