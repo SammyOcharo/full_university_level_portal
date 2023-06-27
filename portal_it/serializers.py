a20f53b8b7d0a09070578ceb04e09e8cee058ff3
sammy ocharo <obanyisammy@gmail.com>
2023-06-27 17:41:43 +0300

    create it app and add views and serializers

---
+++ portal_it/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+class AdminCreateITAdminSerializer(serializers.Serializer):
+    pass
+
+class AdminApproveITAdminSerializer(serializers.Serializer):
+    pass
+
+class AdminSuspendITAdminSerializer(serializers.Serializer):
+    pass
+
+class AdminDeactivateITAdminSerializer(serializers.Serializer):
+    pass
+
+class AdminReactivateITAdminSerializer(serializers.Serializer):
+    pass
+
+class AdminDeleteITAdminSerializer(serializers.Serializer):
+    pass
