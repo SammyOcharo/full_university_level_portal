5a5e56ad89ac0f1a4a3be06772d588f034cb85a3
sammy ocharo <obanyisammy@gmail.com>
2023-06-27 17:45:10 +0300

    create it app and add views and serializers

--- portal_it/views.py
+++ portal_it/views.py
@@ -8,27 +8,45 @@ class AdminCreateITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminCreateITAdminSerializer
 
+    def post(self, request):
+        pass
+
 class AdminApproveITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminApproveITAdminSerializer
+
+    def post(self, request):
+        pass
 
 
 class AdminSuspendITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminSuspendITAdminSerializer
 
+    def post(self, request):
+        pass
+
 
 class AdminDeactivateITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminDeactivateITAdminSerializer
+
+    def post(self, request):
+        pass
 
 
 class AdminReactivateITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminReactivateITAdminSerializer
 
+    def post(self, request):
+        pass
+
 
 
 class AdminDeleteITAdminAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AdminDeleteITAdminSerializer
+
+    def post(self, request):
+        pass
